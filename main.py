bb4a3582affc351b8d2820857f8d33450d7cfcee
vishal <vishalkurre28@gmail.com>
2023-01-31 21:48:00 -0800

    first

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify, render_template
 import os
+import templates
 
-app = Flask(__name__)
+# app = Flask(__name__  )
+app = Flask(__name__  , template_folder='templates')
+
+template_folder='templates'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-index.html
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
