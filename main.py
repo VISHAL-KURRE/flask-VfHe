51047d6897bffc152d45f8516b557c125475bb9a
vishal <vishalkurre28@gmail.com>
2023-01-31 20:19:21 -0800

    first

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
-
+index.html
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
