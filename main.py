5f260d8e8b396d7c6996c8e59e0358f8ef7f41c2
vishal <vishalkurre28@gmail.com>
2023-02-05 14:59:13 -0800

    updated.

--- main.py
+++ main.py
@@ -15,14 +15,8 @@ app = Flask(__name__  , template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = "My secret key"
 template_folder='templates'
 
-# ozekilibsrest messages
-configuration = Configuration(
-    username="http_user",
-    password="qwe123",
-    api_url="http://127.0.0.1:9509/api"
-)
-api = MessageApi(configuration)
-logs = []
+
+
 
 
 
@@ -52,12 +46,7 @@ def index():
         project = str(_form["project"])
         message = str(_form["message"])
     to_address = "+1 5419080576"
-    message = Message(
-        to_address=request.form['to_address'],
-        text=request.form['text']
-    )
-    log = api.send(message)
-    logs.append(log)
+
 
     return render_template('index.html')
 
